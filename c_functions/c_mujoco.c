ccf10294020e2e942664bbf3ebde2c22da66333a
Lucas de Freitas <lukasfreitas8@gmail.com>
2022-05-13 21:50:32 -0300

    atu

---
+++ c_functions/c_mujoco.c
@@ -0,0 +1,15 @@
+#include "include/glfw3.h"
+#include "include/mjdata.h"
+#include "include/mjmodel.h"
+#include "include/mjrender.h"
+#include "include/mjui.h"
+#include "include/mjvisualize.h"
+#include "include/mjxmacro.h"
+#include "include/mujoco.h"
+#include "include/uitools.h"
+
+#include <stdio.h>
+
+int square(int i) {
+	return i * i;
+}
